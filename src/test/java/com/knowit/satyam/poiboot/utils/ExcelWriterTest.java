281afdd480e942c112af8d5d90df65c0db637b53
neo182 <meet_neo1@gmail.com>
2020-04-14 23:27:26 +0200

    More tests and refactorering

--- src/test/java/com/knowit/satyam/poiboot/utils/ExcelWriterTest.java
+++ src/test/java/com/knowit/satyam/poiboot/utils/ExcelWriterTest.java
@@ -1,5 +1,9 @@
 package com.knowit.satyam.poiboot.utils;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.knowit.satyam.poiboot.annotation.ExcelData;
+import com.knowit.satyam.poiboot.validator.ValidationException;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -15,5 +19,18 @@ class ExcelWriterTest {
     public void testWriteToFile() {
         List<Person> list = Arrays.asList(person1, person2);
         ExcelWriter.writeTo(list, "/tmp/test.xlsx");
+    }
+
+    @ExcelData
+    class InvaliClass {
+        String name = "test";
+    }
+
+    @Test
+    public void testWriteToFile_InvalidClass() {
+        assertThatThrownBy(()-> {
+            List<InvaliClass> list = Arrays.asList(new InvaliClass());
+            ExcelWriter.writeTo(list, "/tmp/test.xlsx");
+        }).isInstanceOf(ValidationException.class);
     }
 }
